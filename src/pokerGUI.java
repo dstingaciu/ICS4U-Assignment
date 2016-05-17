abec7d1e8ebb6e5553563319a74dacc76f31c24f
zap12341 <stingaciu.sebastian@gmail.com>
2016-05-17 09:59:06 -0400

    First time

---
+++ src/pokerGUI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+public class pokerGUI {
+	JFrame frame;
+	JPanel panel;
+	JLabel name1,name2,winner;
+	JButton shuffleB;
+	
+	
+	public pokerGUI(){
+		frame=new JFrame("Poker");
+		panel=new JPanel();
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
