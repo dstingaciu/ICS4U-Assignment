d70e8e2f4c8314e42a27472721aa5ed8d352c4a6
zap12341 <stingaciu.sebastian@gmail.com>
2016-05-18 09:58:28 -0400

    More gui

--- src/pokerGUI.java
+++ src/pokerGUI.java
@@ -1,14 +1,31 @@
 import javax.swing.*;
-public class pokerGUI {
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class pokerGUI implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	JLabel name1,name2,winner;
 	JButton shuffleB;
-	
+	int n=1;
 	
 	public pokerGUI(){
 		frame=new JFrame("Poker");
 		panel=new JPanel();
+		panel.setLayout(new GridLayout(0,3,20,20));
+		panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
+		
+	}
+	
+	public void displayName(String p1Name,String p2Name){
+		name1=new JLabel(p1Name);
+		name2=new JLabel(p2Name);
+	}
+	//Output cards based on file names array
+	public void displayHand(String []filenames){
+		for(int i=0;i<filenames.length;i++){
+			
+		}
 	}
 	
 	public static void main(String[] args) {
